0fb469e1df8230572f13315664cfbda190ff785c
rgm94 <rgmartos1994@gmail.com>
2026-05-28 15:46:49 +0200

    Correción archivo de rutas web.php para mejor estructura

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use Symfony\Component\HttpKernel\Profiler\Profile;
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,13 +10,23 @@ Route::get('/', function () {
 
 
 Route::middleware('auth')->group(function () {
+
+    //Rutas del Perfil de usuario
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', [AppointmentController::class, 'index'])
+        ->middleware(['auth', 'verified'])
+        ->name('dashboard');
+
+    //Rutas de Citas
+    Route::controller(AppointmentController::class)->group(function () {
+        Route::get('/appointments/create', 'create')->name('appointments.create');
+        Route::post('/appointments', 'store')->name('appointments.store');
+    });
 });
 
-Route::get('/dashboard', [AppointmentController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
-Route::get('/appointments/create', [AppointmentController::class, 'create'])->name('appointments.create');
-Route::post('/appointments', [AppointmentController::class, 'store'])->name('appointments.store');
+
 
 require __DIR__ . '/auth.php';
